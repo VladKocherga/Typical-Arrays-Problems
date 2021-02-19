ded6058dc4c12c3a978d0c691648eb88de5a2692
VladKocherga <67685279+VladKocherga@users.noreply.github.com>
2021-02-19 21:56:42 +0300

    Init: start Task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
+const { underline } = require("colors");
 
 exports.min = function min (array) {
-  return 0;
+    
+    if ((array === undefined) || (array.length === 0 ))
+    {
+        return 0;
+    }
+    else
+ {  
+    let min = array[0];
+  for (let i = 0; i<array.length; i++)
+  { 
+      if (min > array[i])
+      {
+          min = array[i];
+      }
+      
+  }
+  return min;
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+
+ if ((array === undefined) || (array.length === 0 ))
+ {
+     return 0;
+ }
+ else 
+ {
+    let max = array[0];
+ 
+ for (let k = 0; k < array.length; k++)
+ {
+     if (max < array[k])
+     {
+         max = array[k];
+     }
+ }
+ return max;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0)
+    {
+        return 0;
+    }
+    else
+    {   let sumArray = 0;
+        let avg = 0;
+        for (let i = 0; i<array.length; i++)
+        {
+            sumArray += array[i];
+
+        }
+        avg = sumArray / array.length;
+        return avg;
+    }
+  
 }
